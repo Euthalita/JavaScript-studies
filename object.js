a2e26c6b5fd5b746442b996628bf9888d725c3ce
Thalita <108842621+Euthalita@users.noreply.github.com>
2022-08-30 22:24:12 -0300

    about object

--- object.js
+++ object.js
@@ -1,10 +1,11 @@
+//O tipo de dados mais importantes de JavaScript é o objeto
 //Tudo que possui chaves é um objeto
 //O objeto é uma coleção de chave e valor
 //Dentro de um objeto pode haver outros objetos
 const prod1 = {}
 
 //Podemos definir o objeto, atribuindo atributos e comportamentos. Veja:
-prod1.nome = 'Celular ultra mega' //O objeto terá o atributo nome e o valor 'Celular ultra mega'
+prod1.nome = 'Celular ultra mega' //O objeto terá a propriedade "nome" e o valor 'Celular ultra mega'
 prod1.valor = 4.122
 console.log(prod1)
 
@@ -20,3 +21,7 @@ const prod2 = {
     preco: 79.90
 }
 console.log(prod2)
+
+//Para acessar as propriedades de um objeto, há duas formas
+console.log(prod2["nome"])
+console.log(prod2.nome)
