9c5d23e2c86301bf2a821e4eff076a133dd6011b
Thalita <108842621+Euthalita@users.noreply.github.com>
2022-08-31 12:17:16 -0300

    null and undefined differences

---
+++ null&undefined.js
@@ -0,0 +1,9 @@
+let valor //Não inicializada
+//Quando a variavel não é inicializada, ou seja, não recebe nenhum valor, ela é undefined(indefinida)
+console.log(valor)
+
+valor = null //O null está afirmando que esta variavel não aponta pra nenhum valor na memoria, ela foi inicializa, porem sem valor
+console.log(valor)
+
+//O null é usado geralmente para uma variavel de referencia e que você não quer mais q ela aponte para determinado valor na memoria, ou seja, para zerar uma variavel
+//Não é possivel acessar valores de uma variavel nula
