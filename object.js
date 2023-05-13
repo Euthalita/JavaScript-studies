fc57f54289e5be0171d1bc5e1c0296a85b9d35ae
Thalita <108842621+Euthalita@users.noreply.github.com>
2023-05-12 22:20:11 -0300

    Update object.js
    about object

--- object.js
+++ object.js
@@ -1,4 +1,4 @@
-//O tipo de dados mais importantes de JavaScript é o objeto
+//O tipo de dado mais importante de JavaScript é o objeto
 //Tudo que possui chaves é um objeto
 //O objeto é uma coleção de chave e valor
 //Dentro de um objeto pode haver outros objetos
@@ -9,7 +9,7 @@ prod1.nome = 'Celular ultra mega' //O objeto terá a propriedade "nome" e o valo
 prod1.valor = 4.122
 console.log(prod1)
 
-//Será impresso o nome e o valor, mas não é possivel identificar o nome do objeto sendo prod1 na impressão
+//Dessa forma será impresso somente o valor, mas não é possivel identificar o nome do objeto sendo prod1 na impressão
 
 //É possivel criar um objeto dessa forma também
 prod1['Desconto'] = 0.40
