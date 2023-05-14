d1a649b7266b7eaf6040b9e36460f6bd92083513
Thalita <108842621+Euthalita@users.noreply.github.com>
2023-05-13 22:05:49 -0300

    Update function.js

--- function.js
+++ function.js
@@ -1,5 +1,6 @@
 /*Tudo em JavaScript é função 
-Uma função define um bloco, delimitado por chaves, que tem as sentenças de códigos que serão executados, quando for utilizar aquelas funções, retornando ou não o resultado, normalmente as funções retornam resultados
+Uma função define um bloco, delimitado por chaves, que tem as sentenças de códigos que serão executados, quando for utilizar aquelas funções,
+retornando ou não o resultado, normalmente as funções retornam resultados
 Toda função é um bloco de código nomeado(com nome)
 */
 
@@ -7,7 +8,9 @@ console.log(typeof Object)//objeto é uma função
 class Produto{}
 console.log(typeof Produto)//Classe é uma função
 
-//Para criar uma função, sempre usa-se o function e o nome da função e dentro dos parenteses, colocar os paremetros de entrada, ou seja, os valores que eles
+//Para criar uma função, sempre usa-se o function e o nome da função e dentro dos parenteses, colocar os paremetros de entrada, ou seja,
+os valores que eles
+
 function firstFunctio(a, b){//Essa função é sem retorno, ou seja, não retorna nada
     console.log(a + b)
 }
